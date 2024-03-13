daae0a7c49277f4e9167c99b32eddf7fe52e4715
TheWisePigeon <zozozozeph@gmail.com>
2024-03-13 15:55:29 +0000

    isogram

--- exercism/armstrong_number/main.c
+++ exercism/armstrong_number/main.c
@@ -3,11 +3,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int num_digits(int num) {
+  int count = 0;
+  while (num != 0) {
+    count++;
+    num /= 10;
+  }
+  return count;
+}
+
 bool is_armstrong_number(int candidate) {
-  char candidate_str[20];
+  int len = num_digits(candidate);
+  char candidate_str[len + 1];
   sprintf(candidate_str, "%d", candidate);
   int sum = 0;
-  int len = 0;
   while (candidate_str[len] != '\0') {
     ++len;
   }
@@ -18,6 +27,7 @@ bool is_armstrong_number(int candidate) {
 }
 
 int main(int argc, char *argv[]) {
-  bool result = is_armstrong_number(153);
+  bool result = is_armstrong_number(9);
+  printf("%d", result);
   return EXIT_SUCCESS;
 }
